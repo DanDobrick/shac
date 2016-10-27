01863b4b3c8c01d024e5626b19090fd1c1f0c38b
Dan Dobrick <dandobrick@gmail.com>
2016-10-27 15:08:21 -0500

    add confirmation email requirement to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :students
   devise_for :users
   root to: "application#index"
+
+  devise_for :users, controllers: { confirmations: 'confirmations' }
 end
