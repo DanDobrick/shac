2292e3c38c231e2e7ddec586913da6408a39d28d
Dave Nestoff <d.nestoff@gmail.com>
2016-10-28 12:55:02 -0500

    Complete virtual attributes in student model for average rating as a crasher and a host

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :crash
+  belongs_to :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,8 @@ class Student < ActiveRecord::Base
   belongs_to :user, class_name: 'User', foreign_key: :devise_id
   has_many :hosted_crashes, class_name: 'Crash', foreign_key: :host_id
   has_many :crashes, foreign_key: :crasher_id
+  has_many :reviews_written, foreign_key: :reviewer_id
+  has_many :reviews, foreign_key: :reviewee_id
 
   def full_name
     first_name + ' ' + last_name
@@ -13,4 +15,31 @@ class Student < ActiveRecord::Base
     end
     crashes.compact
   end
+
+  def average_crasher_rating
+    reviews = all_crasher_reviews
+    total_score = reviews.inject (0) {|sum, review| sum + review.rating}
+    (total_score / reviews.count).round(2)
+  end
+
+  def average_host_rating
+    reviews = all_host_reviews
+    total_score = reviews.inject (0) {|sum, review| sum + review.rating}
+    (total_score / reviews.count).round(2)
+  end
+
+  private
+  
+  def all_crasher_reviews
+    reviews.select do |review| 
+      review.crash.crasher_id == self.id
+    end
+  end
+
+  def all_host_reviews
+    reviews.select do |review| 
+      review.crash.host_id == self.id
+    end
+  end
+
 end
