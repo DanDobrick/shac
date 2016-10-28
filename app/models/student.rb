b93eedb2471c931139ac8af08ee4ffecfb92455e
Dan Dobrick <dandobrick@gmail.com>
2016-10-27 19:36:59 -0500

    add full name virtual attribute

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student < ActiveRecord::Base
   belongs_to :user, class_name: 'User', foreign_key: :devise_id
   has_many :hosted_crashes, class_name: 'Crash', foreign_key: :host_id
   has_many :crashes, foreign_key: :crasher_id
+
+  private
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
