22b9e314b64acb95763f7a463c6fea19fece64c2
Dave Nestoff <d.nestoff@gmail.com>
2017-01-23 23:52:27 -0600

    Configure user model and migrations to work with devise token auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_validation :set_provider
+  before_validation :set_uid
   validate :student_email_address
   has_one :student, foreign_key: :devise_id
 
   private
+
+  def set_provider
+    self[:provider] = "email" if self[:provider].blank?
+  end
+
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
 
   def student_email_address
     unless self.email.include?('.edu')
