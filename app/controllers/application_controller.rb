c411bdd2ca657938d80f7f3b0b4e4e7e0ec19fb9
Dave Nestoff <d.nestoff@gmail.com>
2017-01-23 23:51:08 -0600

    Create methods in appliation controller for sanitizing sign_up and user update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::API
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  include ActionController::RequestForgeryProtection
   include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  respond_to :json
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :email])
+  end
+
 
   def index
   end
