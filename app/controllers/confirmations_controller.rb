3cc69d9636f13df5302dafccea83bff49243d8a1
Dave Nestoff <d.nestoff@gmail.com>
2017-01-23 23:51:33 -0600

    Configure all controllers to respond to json

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,4 +1,5 @@
 class ConfirmationsController < Devise::ConfirmationsController
+
   private
   def after_confirmation_path_for(resource_name, resource)
     new_student_path
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
   protected
 
   def after_sign_up_path_for(resource)
